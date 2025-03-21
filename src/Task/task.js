8c6a7906e30f8f7d9c70c81f636b8c34aadfa3ec
Megha Raut <meghakumariraut064@gmail.com>
2025-03-21 12:30:08 +0530

    First massage

--- src/Task/task.js
+++ src/Task/task.js
@@ -7,47 +7,67 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Box, IconButton } from '@mui/material';
-import Search from "../Search/Search";
-
-
-
+import Search from '../Search/Search';
+import Box from '@mui/material/Box';
 
 const columns = [
-  { id: 'SN', label: ' SI. No', minWidth: 30 },
-  { id: 'ProjectTittle', label: ' Project Tittle', minWidth: 70 },
-  { id: 'Assignee', label: 'Assignee', minWidth: 70 },
-  {  id: 'StartDate', label: 'Project Start date',  minWidth: 70,  align: 'right', format: (value) => value.toLocaleString('en-US'), },
-  { id: 'EndDate',label: 'Project end Date',minWidth: 70, align: 'right', format: (value) => value.toLocaleString('en-US'), },
+  {
+    id: 'Sln',
+    label: 'Sl.no',
+    format: (value) => value.toLocaleString('en-US'),
+  },
   
-  { id: 'Priority', label: ' Priority', minWidth: 70 },
-  { id: 'ProjectStatus', label: ' Project Status', minWidth: 70 },
-  { id: 'Action', label: ' Action', minWidth: 100 },
+  { id: 'name', label: 'Project Tittle', minWidth: 170 },
+  
+  {
+    id: 'assigne',
+    label: 'Project Assigne',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'Sdate',
+    label: 'Project Start Date',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'edate',
+    label: 'Project End Date',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+ 
+  {
+    id: 'Priority',
+    label: 'Priority',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'Status',
+    label: 'Project Status',
+    format: (value) => value.toLocaleString('en-US'),
+    
+},
+{
+  id: 'action',
+  label: 'Action',
+  format: (value) => value.toLocaleString('en-US'),
+},
 ];
 
-function createData(SN, ProjectTittle, Assignee, StartDate, EndDate,  Priority, ProjectStatus) {
-  //const density = population / size;
-  return { SN, ProjectTittle, Assignee, StartDate, EndDate,  Priority, ProjectStatus, Action:( <><IconButton style={{color:"#000", padding:"4px", transform:"scale(0.8)"}}>
-    <VisibilityIcon />
-  </IconButton>
-  <IconButton style={{color:"#000", padding:"4px", transform:"scale(0.8)"}}>
-    <EditIcon />
-  </IconButton>
-  <IconButton style={{color:"#000", padding:"4px", transform:"scale(0.8)"}}>
-    <DeleteIcon />
-  </IconButton>
-  </>)} 
-   
+function createData( sln, name, assigne, sdate, edate, priority, status, action) 
+{
+  
+  return { sln, name, assigne, sdate, edate, priority, status, action };
 }
 
 const rows = [
-  createData(1,'Ecommerce', 'Ravish Sir', '01/03/2023', '01/09/2025','low','Completed'),
-  createData(1,'Ecommerce', 'Rajat', '01/03/2023', '01/09/2025','low','Completed'),
+  createData('1','Ecommerce', 'Megha','15/03/25','13/04/25','High','Complete',),
+  createData('2','Inventory', 'Anjali','16/03/25','17/04/25','Medium','Complete',),
+  createData('3','Mobile App', 'Kavita','17/03/25','18/04/25','Law','On Hold',),
+  createData('4','Learning', 'Neha','18/03/25','19/04/25','High','On Hold',),
+  createData('5','Media', 'Sonal','14/03/25','16/04/25','High','Complete',),
   
-];
+  
+  ];
 
 export default function StickyHeadTable() {
   const [page, setPage] = React.useState(0);
@@ -63,8 +83,9 @@ export default function StickyHeadTable() {
   };
 
   return (
-    <Box classname="container" > 
-       <Search/>
+    <Box>
+      <Search/>
+    
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
@@ -115,4 +136,4 @@ export default function StickyHeadTable() {
     </Paper>
     </Box>
   );
-} 
+}
